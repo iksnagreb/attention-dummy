c2f4a78abfcdc9506f81c39b9dd000830e065ed3
Christoph Berganski <christoph.berganski@gmail.com>
2024-01-29 14:28:41 +0100

    Make metrics aggregation parametrizable: Select report file and filter

--- metrics.py
+++ metrics.py
@@ -5,10 +5,16 @@ import pandas as pd
 
 # Script entrypoint
 if __name__ == "__main__":
+    # Open the configuration file
+    with open("params.yaml") as file:
+        # Load the configuration from yaml format
+        params = yaml.safe_load(file)["metrics"]
     # Open the report file
-    with open("build/report/estimate_layer_resources_hls.json") as file:
+    with open(params["report"]) as file:
         # Load the JSON formatted report
         report = pd.read_json(file, orient="index")
+    # Filter the reported rows according to some regex filter rule
+    report = report.filter(regex=params["filter"], axis="rows")
     # Generate a summary of the total resources
     summary = report.sum()
     # Dump the metrics dictionary as yaml
