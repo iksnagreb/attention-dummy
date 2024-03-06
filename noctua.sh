4f950185aafa21c39a77b687e6cdf30e9f23adb1
Christoph Berganski <christoph.berganski@gmail.com>
2024-03-06 15:46:16 +0100

    Fix sbatch job execution to actually work on Noctua 2

---
+++ noctua.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Setup the python environment for model training and evaluation and export
+module load lang/Python/3.10.4-GCCcore-11.3.0
+python3.10 -m venv /dev/shm/env/
+source /dev/shm/env/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Setup the FPGA development environment
+module load fpga
+module load xilinx/xrt/2.14
+module load xilinx/vitis/22.2
+
+# Prepare for running FINN in Singularity containers
+module load system singularity
+export SINGULARITY_CACHEDIR=/dev/shm/singularity-cache/
+export SINGULARITY_TMPDIR=/dev/shm/singularity-tmp/
+export FINN_SINGULARITY=$PC2DATA/hpc-prf-ekiapp/FINN_IMAGES/xilinx/finn_dev.sif
+
+# Prepare FINN to find the Vitis/Vivado installation
+export FINN_XILINX_PATH=/opt/software/FPGA/Xilinx/
+export FINN_XILINX_VERSION=2022.2
+
+# Somehow these options are required to get FINN running on the cluster...
+export LC_ALL="C"
+export PYTHONUNBUFFERED=1
+export XILINX_LOCAL_USER_DATA="no"
+
+# If a path to a FINN installation is specified, move it to some faster storage
+# location
+if [[ -d "$FINN" ]]; then
+  # Copy FINN to the ramdisk
+  cp -r "$FINN" /dev/shm/finn/
+  # Redirect the path specified via environment variable to use the copy
+  export FINN=/dev/shm/finn/
+fi;
+
+# Generarw FINN build outputs and temporaries to the ramdisk
+export FINN_HOST_BUILD_DIR=/dev/shm/finn-build
+
+# Write the command line to be executed to the log
+echo "$@"
+# Forward all command line arguments as the command line to be run as the job
+eval "$@"
+
+# If FINN actually produced build outputs
+if [[ -d "$FINN_HOST_BUILD_DIR" && $DEBUG ]]; then
+  # Generate a (hopefully) unique name for debugging output
+  DEBUG_OUTPUT="build-$(hostname)-$(date +'%Y-%m-%d-%H-%M-%S').tar.gz"
+  # For debugging purposes collect all build outputs from the ramdisk
+  tar -zcf "$DEBUG_OUTPUT" /dev/shm/finn-build
+fi;
--- run.sh
+++ run.sh
@@ -2,14 +2,35 @@
 
 # Is specified via environment variable, execute commands in a job scheduled via
 # sbatch
-if [ "$RUN_SBATCH" = 1 ]; then
+if [ "$RUN_ON_NOCTUA" = 1 ]; then
+  # Specify a name for the SLURM job
+  JOB_NAME="-J ${JOB_NAME:=run}"
+  # Hard time limit of the job, will forcefully be terminated if not done within
+  # this time
+  TIME_LIMIT="-t ${TIME_LIMIT:=01:00:00}"
+  # Number of CPUs to use per task
+  NUM_CPUS="--cpus-per-task=${NUM_CPUS:=16}"
+  # Amount of memory to allocate for the job
+  MEM="--mem ${MEM:=64G}"
+  # The partition to which the job is submitted
+  PARTITION="-p ${PARTITION:=normal}"
+  # Notify by mail on all events (queue, start, stop, fail, ...)
+  MAIL="--mail-type FAIL --mail-user christoph.berganski@uni-paderborn.de"
+  # If using GPUS, specify which type of GPU and how many
+  #   Note: Hardcode this to 1, there is no need for more GPUs right now
+  if [[ "$PARTITION" = "-p gpu" ]] || [[ "$PARTITION" = "-p dgx" ]]; then
+    #   Note: Hardcode this to 1, there is no need for more GPUs right now
+    GPUS="--gres=gpu:a100:1"
+  fi;
+  # Group all sbatch command line arguments into one string
+  ARGS="$JOB_NAME $TIME_LIMIT $NUM_CPUS $MEM $PARTITION $MAIL $GPUS"
   # Forward all arguments following the shell script to be executed as the
   # command line inside of another shell script which is executed via sbatch
   #   Note: Waiting/Blocking sbatch with the -W option
-  sbatch -W batch.sh "$@"
+  sbatch $ARGS -W --verbose noctua.sh "$@"
 # By default, execute the job locally
 else
   # Forward all arguments following the shell script to be executed as the
   # command line within this script
-  "$@";
+  eval "$@";
 fi;
